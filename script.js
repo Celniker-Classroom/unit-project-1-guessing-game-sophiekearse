0365cb160f7c992f405f70d831da82aee59b8487
sophiekearse <sophiekearse@gmail.com>
2026-04-07 20:54:33 +0000

    play and buttons

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 // add javascript here
+//Game State
+let answer = 0;
+let guessCount = 0;
+let totalWins = 0;
+let totalGuesses = 0;
+let scores = 0;
+
+//player name
+let playerName = prompt("Enter your name:");
+
+//play
+
+
+//get level
+document.getElementById("playBtn").addEventListener("click", function() {
+    let radios = document.getElementsByName("level");
+    let range = 3;
+    for (let i = 0; i < radios.length; i++){
+        if(radios[i].checked) {
+            range  = parseint(radios[i].value);
+        }
+    }
+
+//pick answer
+answer = Math.floor(Math.random() * range) + 1;
+
+//disable and enable buttons and radio choices
+document.getElementById("msg").textContent = playerName + ", guess a number between 1 " + range;
+document.getElementById("guess").value="";
+document.getElementById("guessBtn").disabled = false;
+document.getElementById("giveUpBtn").disabled = false;
+document.getElementById("playBtn").disabled = true;
+
+let levelRadios = document.getElementsByName("level");
+for (let i = 0; i < radios.length; i++) {
+    levelRadios[i].disabled = true;
+}
+
+});
+
+
