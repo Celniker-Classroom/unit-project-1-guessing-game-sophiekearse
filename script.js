954365e54e995e18812545f9d052685b1028bd18
sophiekearse <sophiekearse@gmail.com>
2026-04-08 03:12:49 +0000

    added temps and guess tips

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ document.getElementById("playBtn").addEventListener("click", function() {
     }
 
 //pick answer
-answer = Math.floor(Math.random() * range) + 1;
+let answer = Math.floor(Math.random() * range) + 1;
 
 //disable and enable buttons and radio choices
-document.getElementById("msg").textContent = playerName + ", guess a number between 1 " + range;
+document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
 document.getElementById("guess").value="";
 document.getElementById("guessBtn").disabled = false;
 document.getElementById("giveUpBtn").disabled = false;
@@ -40,3 +40,39 @@ for (let i = 0; i < levelRadios.length; i++) {
 });
 
 
+//guessing
+document.getElementById("guessBtn").addEventListener("click", function() {
+let guess = Number(document.getElementById("guess").value);
+let temp = "";
+
+if (Math.abs(guess - answer) <= 2) {
+            temp = "hot"; 
+}       else if (Math.abs(guess - answer) <= 5) {
+                    temp = "warm";
+             } else {
+                    temp = "cold";
+             }
+
+if (guess > answer) {
+    document.getElementById("msg").innerText = "Too high, " + temp;
+    
+} else if (guess < answer) {
+        document.getElementById("msg").innerText = "Too low, " + temp;
+        }
+        else {
+            document.getElementById("msg").innerText = "Correct! The answer was " + answer;
+            totalWins++;
+            totalGuesses += guessCount + 1;
+            scores = totalWins / totalGuesses;
+            document.getElementById("score").innerText = "Score: " + scores.toFixed(2);
+            document.getElementById("guessBtn").disabled = true;
+            document.getElementById("giveUpBtn").disabled = true;
+            document.getElementById("playBtn").disabled = false;
+
+            let levelRadios = document.getElementsByName("level");
+            for (let i = 0; i < levelRadios.length; i++) {
+                levelRadios[i].disabled = false;
+            }
+        }
+
+    });
